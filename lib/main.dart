101681c8a65f6c08222e8cf21ab1248ffd92c9f3
olaomar2000 <olashab300@gmail.com>
2021-06-24 11:20:47 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ListView1(),
+  ));
+}
+
+class ListView2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView.builder'),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView.builder(
+        itemCount: 8,
+        itemBuilder: (BuildContext con, int item) {
+          return ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('GFG'),
+            title: Text('ListItem $item'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ListView1 extends StatelessWidget {
+  List name = ['bike', 'boat', 'bus', 'car', 'railway', 'run'];
+
+  List icons = [
+    Icons.directions_bike_outlined,
+    Icons.directions_boat,
+    Icons.directions_bus,
+    Icons.directions_car,
+    Icons.directions_railway,
+    Icons.directions_run
+  ];
+  // git config --global user.email "you@example.com"
+  // git config --global user.name "Your Name"
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListViews'),
+        backgroundColor: Colors.teal,
+      ),
+      body: ListView.builder(
+        itemCount: name.length,
+        itemBuilder: (BuildContext con, int item) {
+          return Card(
+            child: ListTile(
+              leading: Icon(icons[item]),
+              title: Text(name[item]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
